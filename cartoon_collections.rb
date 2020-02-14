116c8cfd4149a8110758c474b82c42a5230cddc9
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-14 01:01:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,10 @@ def summon_captain_planet(array)
    
 end
 
-def long_planeteer_calls(# code an argument here)
+def long_planeteer_calls(array)
   # Your code here
+  
+  
 end
 
 def find_the_cheese# code an argument here
