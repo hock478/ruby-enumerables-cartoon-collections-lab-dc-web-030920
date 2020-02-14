af6dd4c127190f7f51597d7c6a99dc635f532aaa
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-14 01:08:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,10 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  
+  result = nil
+  array.each do |cheese|
+  if sharks.include? "a"
+    result = shark
+    break
+  end
 end
